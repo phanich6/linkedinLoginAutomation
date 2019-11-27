f6e6fcc7e484fe69ef9197dbdd361adc0aad4931
phanich6 <58245138+phanich6@users.noreply.github.com>
2019-11-27 14:25:52 +0530

    Logout updated

--- src/pages/HomePage.java
+++ src/pages/HomePage.java
@@ -1,41 +1,43 @@
 package pages;
 
-	
-	import java.util.Set;
+import java.util.Set;
 
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.support.FindBy;
-	import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-	public class HomePage {
+public class HomePage {
 
-		WebDriver driver;
+	WebDriver driver;
 
-		@FindBy(xpath = "//a[text()='Write an article']")
-		WebElement writeArticleLink;
+	@FindBy(xpath = "//a[text()='Write an article']")
+	WebElement writeArticleLink;
 
-		@FindBy(xpath = "//button[@data-control-name='nav.settings']")
-		WebElement Settings;
+	@FindBy(xpath = "//button[@data-control-name='nav.settings']")
+	WebElement Settings;
 
-		@FindBy(xpath = "//a[@data-control-name='nav.settings_signout']")
-		WebElement SignOut;
+	@FindBy(xpath = "//a[@data-control-name='nav.settings_signout']")
+	WebElement SignOut;
 
-		public HomePage(WebDriver driver) {
-			this.driver = driver;
-			PageFactory.initElements(driver, this);
+	public HomePage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+
+	public WriteArticlePage clickWriteArticle() {
+
+		writeArticleLink.click();
+		Set<String> handles = driver.getWindowHandles();
+		for (String win : handles) {
+			driver.switchTo().window(win);
 		}
+		return new WriteArticlePage(driver);
+	}
 
-		public WriteArticlePage clickWriteArticle() {
+	public void logout() throws InterruptedException {
+		Settings.click();
+		SignOut.click();
 
-			writeArticleLink.click();
-			Set<String> handles = driver.getWindowHandles();
-			for (String win : handles) {
-				driver.switchTo().window(win);
-			}
-			return new WriteArticlePage(driver);
-		}
-	
-	
-	
+	}
 }
